64be1ed79bde22e48e75ebd6f095053981490396
tramngyn <bvnguyen115@student.hbuhsd.edu>
2025-03-06 11:04:22 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,17 +4,23 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Test code here
+    int[][] matrix = { {1,2,3}, {4,5,6}, {7,8,9} };
+    sumofDiag(matrix);
   }
 
 
   public static int sumOfDiag(int[][] matrix)
   {
-    return 0;
+    int sum = 0; 
+    for (int r = 0; r < matrix.length; r++)
+    {
+      sum += matrix[r][r];
+    }
+    return sum;
   }
 
   public static int[][] productTable(final int numRows, final int numCols)
   {
-    return null;
+    
   }
 }
