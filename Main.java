d9c8fbfe07e5908ab4d842408b0819d428d9538a
tramngyn <bvnguyen115@student.hbuhsd.edu>
2025-03-06 11:06:10 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main
   public static void main(String[] args)
   {
     int[][] matrix = { {1,2,3}, {4,5,6}, {7,8,9} };
-    sumofDiag(matrix);
+    sumOfDiag(matrix);
   }
 
 
@@ -19,7 +19,7 @@ public class Main
     return sum;
   }
 
-  public static int[][] productTable(final int numRows, final int numCols)
+    public static int[][] productTable(final int numRows, final int numCols)
   {
     
   }
